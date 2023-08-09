9a323a686682ed0d6a3bce05e74546a69f078ac4
Salymbek <calumbekcaatbekov@gmail.com>
2023-08-09 15:52:14 +0600

    update

---
+++ src/main/java/peaksoft/house/gadgetariumb9/dto/request/review/ReviewUserRequest.java
@@ -0,0 +1,24 @@
+package peaksoft.house.gadgetariumb9.dto.request.review;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewUserRequest {
+
+  private Long reviewId;
+
+  private String comment;
+
+  private int grade;
+
+  private String imageLink;
+
+}
